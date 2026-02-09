7edc8d420b7e7aa02e2eab96cc60315910b7df6b
ladan-13570 <136787148+ladan-13570@users.noreply.github.com>
2026-02-08 21:45:23 -0500

    added moment.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,23 @@
-let losAngelesElementor = document.querySelector("#los-angeles");
 
-losAngelesElementor.innerHTML = "Happy coding";
+function updateTime() {
+  // Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    "h:mm:ss [<small>]A[</small>]",
+  );
+  //Sydney
+  let SydneyElement = document.querySelector("#Sydney");
+  let SydneyDateElement = SydneyElement.querySelector(".date");
+  let SydneyTimeElement = SydneyElement.querySelector(".time");
+  let SydneyTime = moment().tz("Australia/Sydney");
+
+  SydneyDateElement.innerHTML = SydneyTime.format("MMMM Do YYYY");
+
+  SydneyTimeElement.innerHTML = SydneyTime.format(
+    "h:mm:ss [<small>]A[</small>]",
+  );
+}
+
+updateTime();
+setInterval(updateTime,1);
+
