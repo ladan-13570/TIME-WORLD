216e7fb08e54b4a35d2fe19b45892dfc5cff9b30
ladan-13570 <136787148+ladan-13570@users.noreply.github.com>
2026-02-08 21:46:01 -0500

    added time zone

--- javascript/index.js
+++ javascript/index.js
@@ -3,6 +3,12 @@ function updateTime() {
   // Los Angeles
   let losAngelesElement = document.querySelector("#los-angeles");
   let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
     "h:mm:ss [<small>]A[</small>]",
   );
   //Sydney
